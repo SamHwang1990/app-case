fdeabd97cc1284ddd3b3bdb5d54f5b9bdefec1ed
SamHwang1990 <samhwang1990@gmail.com>
2014-09-16 17:43:01 +0800

    modify Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function(grunt){
             }
         },
         watch: {
-            files: ['<%= jshint.files %>'],
+            files: jshint.files,
             tasks: ['jshint', 'karma'],
             options: {
                 spawn: false,
