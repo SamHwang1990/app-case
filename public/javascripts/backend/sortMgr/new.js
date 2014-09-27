439d8f78c36aec682c7746cacb7746ba7d5f7a80
SamHwang1990 <samhwang1990@gmail.com>
2014-09-27 18:25:46 +0800

    next is to write the model proxy method

--- public/javascripts/backend/sortMgr/new.js
+++ public/javascripts/backend/sortMgr/new.js
@@ -5,8 +5,41 @@
 (function(){
 
 	//组合Sort Info的编辑表单
-	var genEditFormHtml = function(name, slug, description, remark){
+	var genEditFormHtml = function(sortId, name, slug, description, remark){
+		var message =
+		'<div class="row">  ' +
+		'<div class="col-md-12"> ' +
+		'<form role="form" class="sort_editForm" method="post"> ' +
+		'<div class="form-group"> ' +
+		'<label for="name">1. 留学类型名</label>' +
+		'<input type="hidden" class="form-control" id="sort_id" name="sort_id" value="'+ sortId + '" />' +
+		'<input type="text" class="form-control" id="name" name="name" placeholder="留学类型名" value="'+ name + '" />' +
+		'</div> ' +
+		'<div class="form-group"> ' +
+		'<label for="slug">2. 留学类型英文名</label>' +
+		'<input type="text" class="form-control" id="slug" name="slug" placeholder="留学类型英文名" value="'+ slug + '" />' +
+		'</div> ' +
+		'<div class="form-group"> ' +
+		'<label for="description">3. 留学类型描述</label>' +
+		'<input type="text" class="form-control" id="description" name="description" placeholder="留学类型描述，前台展示所用" value="'+ description + '" />' +
+		'</div> ' +
+		'<div class="form-group"> ' +
+		'<label for="remark">4. 留学类型备注</label>' +
+		'<textarea class="form-control" id="remark" name="remark" placeholder="留学类型备注，管理人员所用">'+ remark + '</textarea>' +
+		'</div> ' +
+		'</form> </div>  </div>';
+		return message
+	};
 
+	var genEditData = function(wrapEl){
+		return {
+			id:wrapEl.attr('data-id'),
+			name:wrapEl.attr('data-name'),
+			slug:wrapEl.attr('data-slug'),
+			description:wrapEl.attr('data-description'),
+			grade:wrapEl.attr('data-grade'),
+			remark:wrapEl.attr('data-remark')
+		}
 	};
 
 	//处理删除Sort的box
@@ -35,13 +68,38 @@
 	};
 
 	//处理编辑Sort的box
-	var editBox = function($wrapEl, ajaxUrl, sortId, sortGrade){
-
+	var editBox = function($wrapEl, ajaxUrl){
+		var editData = genEditData($wrapEl);
+		bootbox.dialog({
+			className:'sort_edit_box',
+			message: genEditFormHtml(editData.id, editData.name, editData.slug, editData.description, editData.remark),
+			buttons: {
+				default: {
+					label: "No",
+					className: "btn-default",
+					callback: function() {
+						alert('cencel edit');
+					}
+				},
+				primary: {
+					label: "Yes",
+					className: "btn-primary",
+					callback: function() {
+						alert('edit done');
+						if(editData.grade === '1'){
+							updateSortItemDom($wrapEl,editData);
+						}else if(editData.grade === '2'){
+							updateSortItemOptionDom($wrapEl,editData);
+						}
+					}
+				}
+			}
+		});
 	};
 
 	//更新SortItem的Dom内容
 	var updateSortItemDom = function($wrapEl, updateData){
-
+		$wrapEl.find('.sort_eduType_item_name').text(updateData.name);
 	};
 
 	//更新SortItem-Option的Dom内容
@@ -77,10 +135,13 @@
 		});
 
 		$('.sort_eduType_item_edit').on('click',function(event){
-
+			event.preventDefault();
+			var $itemEl = $(this).parents('.sort_eduType_item');
+			editBox($itemEl,'');
 		});
 
 		$('.sort_eduType_item_remove').on('click',function(event){
+			event.preventDefault();
 			var $itemEl = $(this).parents('.sort_eduType_item');
 			var itemId = $itemEl.attr('data-id');
 			var itemGrade = $itemEl.attr('data-grade');
@@ -92,10 +153,13 @@
 		});
 
 		$('.sort_eduType_item_editOption').on('click',function(event){
-
+			event.preventDefault();
+			var $optionEl = $(this).parents('.sort_eduType_item_option');
+			editBox($optionEl,'');
 		});
 
 		$('.sort_eduType_item_removeOption').on('click',function(event){
+			event.preventDefault();
 			var $optionEl = $(this).parents('.sort_eduType_item_option');
 			var optionId = $optionEl.attr('data-id');
 			var optionGrade = $optionEl.attr('data-grade');
